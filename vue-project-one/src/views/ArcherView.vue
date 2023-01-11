e2424de77582fcaf36f9233a1d1e54e30520b724
Foswa <fcahnevrard@gmail.com>
2023-01-11 18:47:47 +0200

    Most of the functionality is complete.

--- vue-project-one/src/views/ArcherView.vue
+++ vue-project-one/src/views/ArcherView.vue
@@ -3,74 +3,87 @@
 
 </script>
 <template>
-    
-    
-    <div class="unit-showcase-parent" >
-        <div id="unit-selector">
-            
-            <div><img src="../../images/Generic-Archer-Icon.webp" alt="" width="40px"></div>
-            <div><img src="../../images/Generic-Archer-Icon.webp" alt="" width="40px"></div>
-            <div><img src="../../images/Generic-Archer-Icon.webp" alt="" width="40px"></div>
-            <div><img src="../../images/Generic-Archer-Icon.webp" alt="" width="40px"></div> 
-            <!-- <div>
-                <router-link to="/MAA"><img src="images/Generic-Archer-Icon.webp" width="40px"></router-link>
+        
+    <div class="parent">
+        <div class="special-unit-parent">
+            <div class="special-unit-text">
+                <p>Special Units</p>
+            </div>
+            <div class="special-unit-icons">
 
-            </div> -->
-            
-            <!-- <div>
-                <router-link to="/MAA"><img src="images/Generic-Archer-Icon.webp" width="40px"></router-link>
-            </div>  -->
-            
-        </div>
-        <div id="unit-description-parent">
-            <div id="unit-description">
-                <div><img src="images/Generic-Archer-Icon.webp" alt=""></div>
-                <div><p>Archer</p></div>
-                <div><p>Light Ranged Infantry</p></div>   
-                <div><p>French</p></div>
-            </div>
-            <div>
-                <div>Cheap ranged infantry with good damage vs. unarmored targets.<br>+ High rate of fire<br>- Weak against armored targets<br>- Countered by Horsemen</div>
+                <div>
+                    <img src="../../images/Arbaletrier-Icon.webp" style="width:50px">
+                </div>
+                <div>
+                    <img src="../../images/French-RoyalKnight-Icon.webp" style="width:50px">
+                </div>
+                <div>
+                    <img src="../../images/French-Cannon-Icon.webp" style="width:50px">
+                </div>
             </div>
         </div>
-        <div id="unit-role">
-            <div><p>Royal knights are flexible<br>Early game ,Feudal/age2,it can be used for raiding<br><br>
-            If you go royale institute and fast castle you could unlock royal bloodlines while your enemy is in feudal and do alot of damage
-            as they do not have crossbows<br><br>
-            With royal bloodlines they are very strong and can even beat spearmen at equal numbers</p></div>
+        <div class="unit-showcase-parent" >
+            <div id="unit-selector">
+                <div>
+                    <RouterLink to="/Knight">
+                        <img  src="../../images/French-RoyalKnight-Icon.webp" alt="" style="width:40px" >
+                    </RouterLink>
+                </div>
+
+                <div>
+                    <RouterLink to="/MAA">
+                        <img src="../../images/Generic-MAA-Icon.webp" alt="" style="width:40px">
+                    </RouterLink>
+                </div>
+                <div>
+                    <RouterLink to="/spearmen">
+                        <img src="../../images/Generic-Spearmen-Icon.webp" alt="" style="width:40px">
+                    </RouterLink>
+                </div>
+                <div>
+                    <RouterLink to="/Archer">
+                        <img src="../../images/Generic-Archer-Icon.webp" alt="" style="width:40px">
+                    </RouterLink>
+                    
+                </div>
+                <div>
+                    <RouterLink to="/arbaletrier">
+
+                        <img src="../../images/Arbaletrier-Icon.webp" alt="" style="width:40px">
+                    </RouterLink>
+                </div>
+                <div>
+                    <RouterLink to="/handcannoneer">
+                        <img src="../../images/Handcannoneer-Icon.webp" alt="" style="width:40px">
+
+                    </RouterLink>
+                </div>
+                <div><img src="../../images/Horseman-Icon.webp" alt="" style="width:40px"></div>
+                
+            </div>
+            <div id="unit-description-parent">
+                <div id="unit-description">
+                    <div><img src="images/Generic-Archer-Icon.webp" alt=""></div>
+                    <div><p>Archer</p></div>
+                    <div><p>Light Ranged Infantry</p></div>   
+                    <div><p>French</p></div>
+                </div>
+                <div>
+                    <div>Cheap ranged infantry with good damage vs. unarmored targets.
+                        <br>+ High rate of fire
+                        <br>- Weak against armored targets
+                        <br>- Countered by Horsemen
+
+                    </div>
+                </div>
+            </div>
+            <div id="unit-role">
+                <div><p>Royal knights are flexible<br>Early game ,Feudal/age2,it can be used for raiding<br><br>
+                If you go royale institute and fast castle you could unlock royal bloodlines while your enemy is in feudal and do alot of damage
+                as they do not have crossbows<br><br>
+                With royal bloodlines they are very strong and can even beat spearmen at equal numbers</p></div>
+            </div>
         </div>
     </div>
 </template>
-<style >
-
-
-.unit-showcase-parent{
-    display: flex;
-    justify-content: center;
-}
-
-.unit-showcase-parent div{
-    margin: 20px;
-    background-color: brown;
-}
-#unit-description-parent{
-    display: flex;
-    flex-direction: column;
-}
-#unit-description{
-    display: flex;
-    
-}
-
-#unit-description-parent div{
-    background-color: aquamarine;
-}
-
-#unit-role{
-    max-width: 600px;
-}
-
-
-
-
-</style>
+<style></style>
---
+++ vue-project-one/src/views/UnitCard.vue
@@ -0,0 +1,66 @@
+<script>
+
+import frenchSourceData from '../../Data/french.json'
+
+// export default { 
+//     data() {
+//         return {
+//             data: frenchSourceData
+//         }
+//     }
+// }
+
+
+export default {
+    data() {
+        return { 
+            data: frenchSourceData,
+            currentId: this.$route.params.id
+        }
+    }
+}
+
+</script>
+<template>
+    <!-- <div v-for="unit in data" :key="unit.baseId">
+      <router-link :to="{ name: 'unit', params:{ baseId: unit.baseId} }"> </router-link>
+      <p>Hey how are you m     </p>
+    </div> -->
+    <div class="unit-showcase-parent" >
+        <div id="unit-selector">
+            <div><img src="../../images/Knight-Icon.webp" alt="" width="40px"></div>
+            <div><img src="../../images/Generic-MAA-Icon.webp" alt="" width="40px"></div>
+            <div><img src="../../images/Generic-Spearmen-Icon.webp" alt="" width="40px"></div>
+            <div><img src="../../images/Generic-Archer-Icon.webp" alt="" width="40px"></div>
+            <div><img src="../../images/Arbaletrier-Icon.webp" alt="" width="40px"></div>
+            <div><img src="../../images/Handcannoneer-Icon.webp" alt="" width="40px"></div>
+            <div><img src="../../images/Horseman-Icon.webp" alt="" width="40px"></div>
+            
+        </div>
+        <div id="unit-description-parent">
+            <div id="unit-description">
+                <div><img src="images/Knight-Icon.webp" alt=""></div>
+                <div><p>This is the generic template</p></div>
+                <div><p>Heavy melee cavalry</p></div>   
+                <div><p>French</p></div>
+            </div>
+            <div>
+                <div>Gain bonus damage for 5 seconds after charging. Effective against most units.<br>+ Heavy armor<br>+ Strong in melee combat<br>- Countered by Spearmen and Crossbowmen</div>
+            </div>
+        </div>
+        <div id="unit-role">
+            <div><p>Royal knights are flexible<br>Early game ,Feudal/age2,it can be used for raiding<br><br>
+            If you go royale institute and fast castle you could unlock royal bloodlines while your enemy is in feudal and do alot of damage
+            as they do not have crossbows<br><br>
+            With royal bloodlines they are very strong and can even beat spearmen at equal numbers</p></div>
+        </div>
+    </div>
+    <div v-for="unit in data" :key="unit.baseId">
+          <router-link :to="{ name: 'unit', params:{ baseId: unit.baseId} }"> </router-link>
+    </div>
+
+</template>
+<style scoped>
+
+
+</style>
